23a3849cc4af375e7b2786eeb1cf23b902e1e7cc
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-10-03 18:04:09 -0400

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,9 @@ class CorsMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', 'http://cliente.vakajose.online');
+
+        // Permitir cualquier origen
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
